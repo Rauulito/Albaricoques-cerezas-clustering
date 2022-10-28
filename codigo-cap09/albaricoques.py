47445fe95e8af1e70684ade25ef27f564446b2b3
Rauulito <7rnavarro7@gmail.com>
2022-10-28 16:32:46 +0200

    clases separadas

---
+++ codigo-cap09/albaricoques.py
@@ -0,0 +1,31 @@
+#---- IMPORTAR MÓDULOS --
+import random
+import pandas as pnd
+
+#---- CARACTERÍSTICAS------
+
+#ALBARICOQUES: ATENCIÓN DOS CASOS DE PRUEBAS EN FUNCIÓN DEL AVANCE DE SU LECTURA
+
+#Caso 1:
+caracteristicasAlbaricoques = [[40,44,41],[45,49,54],[50,54,74],[55,59,100]]
+
+#Caso 2:
+#caracteristicasAlbaricoques = [[35,39,27],[40,44,41],[45,49,54],[50,54,74],[55,59,100]]
+
+#GENERACION DE LOS DATOS
+# [DIAMETRO, PESO]
+cantidadObservaciones = 200
+#Generación de los albaricoques
+albaricoques = []
+random.seed()
+for iteration in range(cantidadObservaciones):
+    #elección al azar de una característica
+    albaricoque = random.choice(caracteristicasAlbaricoques)
+    #Generación de un diámetro
+    diametro = round(random.uniform(albaricoque[0], albaricoque[1]),2)
+    #Generación de un peso
+    limiteMinPeso = albaricoque[2] / 1.10
+    limiteMaxPeso = albaricoque[2] * 1.10
+    peso = round(random.uniform(limiteMinPeso, limiteMaxPeso),2)
+    print ("Albaricoque "+str(iteration)+" "+str(albaricoque)+" : "+str(diametro)+" - "+str(peso))
+    albaricoques.append([diametro,peso])
---
+++ codigo-cap09/clustering.py
@@ -0,0 +1,17 @@
+#Importaciones necesarias
+import cerezas
+import albaricoques
+import random
+import pandas as pnd
+
+
+#Constitución de las observaciones
+frutas = cerezas+albaricoques
+print(frutas)
+
+#Mezcla de las observaciones
+random.shuffle(frutas)
+
+#Guardado de las observaciones en un archivo
+dataFrame = pnd.DataFrame(frutas)
+dataFrame.to_csv("datas/frutas.csv", index=False,header=False)
