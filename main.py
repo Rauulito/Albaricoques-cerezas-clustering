6b15e01923a3ffe97e40ef12408c782ea5eca7eb
Rauulito <7rnavarro7@gmail.com>
2022-10-30 13:32:43 +0100

    cambios

--- main.py
+++ main.py
@@ -5,12 +5,12 @@ sys.path.insert(0,"/Users/rauln/Documents/Desarrolo orientado a objetos/Albarico
 sys.path.insert(0, "/Users/rauln/Documents/Desarrolo orientado a objetos/Albaricoques-cerezas-clustering/codigo-cap09/generacion_de_datos")
 sys.path.insert(0, "/Users/rauln/Documents/Desarrolo orientado a objetos/Albaricoques-cerezas-clustering/codigo-cap09/Visualizacion_3d_curvas_gaussianas")
 #Importamos funciones
-from aprendizaje_k_mean import 
-from aprendizeje_no_supervisado import 
-from clustering2 import stemming
-from cerezas import lematizacion
-from albaricoques import canalizacion
-from clustering import Info_original
+from aprendizaje_k_mean import
+from aprendizeje_no_supervisado import
+from clustering2 import
+from cerezas import
+from albaricoques import
+from clustering import
 
 
 def iniciar():
@@ -22,26 +22,18 @@ def iniciar():
         print("========================")
         print("  Bienvenido al Albaricoques-Cerezas-Clustering  ")
         print("========================")
-        print("[1] Ejecute el programa")
-        print("[2] Ejecute alguna funcion en concreto")
-        print("[3] Cerrar el Gestor    ")
+        print("[1] Escoga la carpeta y con ello la funcion de ella que quiere escoger")
+        print("[2] Cerrar el Gestor    ")
         print("========================")
 
         opcion = input("> ")
 
         if opcion == '1':
-            print("DETERMINACION OPINIONES TWITTER")
-            pruebas()
-        if opcion == '2':
             while True:
                 print("¿Qué opción quieres escoger?")
                 print("[1] Informacion Transformacion")
                 print("[2] Normalizacion")
                 print("[3] Stopwords")
-                print("[4] Stemming")
-                print("[5] Lematizacion")
-                print("[6] Canalizacion")
-                print("[7] Volver al menu principal")
                 opcion2 = input("> ")
                 if opcion2 == '1':
                     Info_original()
@@ -58,7 +50,7 @@ def iniciar():
                 elif opcion2 == '7':
                     print("Volviendo...")
                     break
-        if opcion == '3':
+        if opcion == '2':
             print("Saliendo...")
             break
     
