d5f4196228df25e14e8fb6adb395d6dd6adf15b4
lorenzoo22 <lagudmarr@gmail.com>
2022-10-30 18:31:39 +0100

    funciones

--- codigo-cap09/generacion_de_datos/cerezas.py
+++ codigo-cap09/generacion_de_datos/cerezas.py
@@ -3,23 +3,23 @@ import random
 import pandas as pnd
 
 #---- CARACTERÍSTICAS------
+def cerezas():
+    #CEREZAS
+    caracteristicasCerezas = [[17,19,1,5],[20,21,5,6],[22,23,6,7],[24,25,7,8.5],[26,27,8.5,10],[28,29,10,11.5]]
 
-#CEREZAS
-caracteristicasCerezas = [[17,19,1,5],[20,21,5,6],[22,23,6,7],[24,25,7,8.5],[26,27,8.5,10],[28,29,10,11.5]]
+    #GENERACION DE LOS DATOS
+    # [DIAMETRO, PESO]
+    cantidadObservaciones = 200
 
-#GENERACION DE LOS DATOS
-# [DIAMETRO, PESO]
-cantidadObservaciones = 200
-
-#Generación de las cerezas
-cerezas = []
-random.seed()
-for iteration in range(cantidadObservaciones):
-    #elección al azar de una característica
-    cereza = random.choice(caracteristicasCerezas)
-    #Generación de un diámetro
-    diametro = round(random.uniform(cereza[0], cereza[1]),2)
-    #Generación de un peso
-    peso = round(random.uniform(cereza[2], cereza[3]),2)
-    print ("Cereza "+str(iteration)+" "+str(cereza)+" : "+str(diametro)+" - "+str(peso))
-    cerezas.append([diametro,peso])
+    #Generación de las cerezas
+    cerezas = []
+    random.seed()
+    for iteration in range(cantidadObservaciones):
+        #elección al azar de una característica
+        cereza = random.choice(caracteristicasCerezas)
+        #Generación de un diámetro
+        diametro = round(random.uniform(cereza[0], cereza[1]),2)
+        #Generación de un peso
+        peso = round(random.uniform(cereza[2], cereza[3]),2)
+        print ("Cereza "+str(iteration)+" "+str(cereza)+" : "+str(diametro)+" - "+str(peso))
+        cerezas.append([diametro,peso])
--- codigo-cap09/generacion_de_datos/clustering.py
+++ codigo-cap09/generacion_de_datos/clustering.py
@@ -4,14 +4,14 @@ import albaricoques
 import random
 import pandas as pnd
 
+def clustering():
+    #Constitución de las observaciones
+    frutas = cerezas.cerezas+albaricoques.albaricoques #Estamos cogiendo las listas de cerezas y albaricoques y las unimos en una sola lista
+    print(frutas)
 
-#Constitución de las observaciones
-frutas = cerezas.cerezas+albaricoques.albaricoques #Estamos cogiendo las listas de cerezas y albaricoques y las unimos en una sola lista
-print(frutas)
+    #Mezcla de las observaciones
+    random.shuffle(frutas)
 
-#Mezcla de las observaciones
-random.shuffle(frutas)
-
-#Guardado de las observaciones en un archivo
-dataFrame = pnd.DataFrame(frutas)
-dataFrame.to_csv("datas/frutas.csv", index=False,header=False)
+    #Guardado de las observaciones en un archivo
+    dataFrame = pnd.DataFrame(frutas)
+    dataFrame.to_csv("datas/frutas.csv", index=False,header=False)
